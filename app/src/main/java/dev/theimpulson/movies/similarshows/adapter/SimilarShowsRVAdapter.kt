a0f8054892a301bf00ca052b5b4c66ed16eb88a5
Aayush Gupta <aayushgupta219@gmail.com>
2022-04-22 10:48:01 +0530

    Movies: SimilarShows: Improve item UI a bit more
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/dev/theimpulson/movies/similarshows/adapter/SimilarShowsRVAdapter.kt
+++ app/src/main/java/dev/theimpulson/movies/similarshows/adapter/SimilarShowsRVAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import coil.transform.BlurTransformation
 import dev.theimpulson.movies.api.TMDBAPIInterface
 import dev.theimpulson.movies.api.data.Show
 import dev.theimpulson.movies.common.ShowDiffUtil
@@ -17,11 +18,14 @@ class SimilarShowsRVAdapter :
         RecyclerView.ViewHolder(binding.root)
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val context = holder.itemView.context
         val show = getItem(position) ?: Show()
 
         holder.binding.apply {
             if (!show.backdrop_path.isNullOrBlank()) {
-                backdropIV.load(TMDBAPIInterface.ASSETS_URL + show.backdrop_path)
+                backdropIV.load(TMDBAPIInterface.ASSETS_URL + show.backdrop_path) {
+                    transformations(BlurTransformation(context))
+                }
             }
             showPosterIV.load(TMDBAPIInterface.ASSETS_URL + show.poster_path)
             showTitleTV.text = show.name
